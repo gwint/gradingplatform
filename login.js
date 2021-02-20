312c4f19eb5452d0ff34bd3e9adee531411b1525
Gregory Wint <gregwint95@gmail.com>
2021-02-20 14:32:42 -0500

    Add bare bones login/upload pages

--- login.js
+++ login.js
@@ -1,1 +1,16 @@
 // login.js
+
+function init() {
+    const button = document.getElementById("loginBtn");
+
+    button.onclick = function() {
+        alert("logging in user");
+        window.location.href = "upload.html";
+    };
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+      // - Code to execute when all DOM content is loaded. 
+      // - including fonts, images, etc.
+    init();
+});
