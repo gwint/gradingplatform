493bc4b8ceb07e15201f66043b42aaddbfa4d936
Gregory Wint <gregwint95@gmail.com>
2021-02-20 16:45:40 -0500

    Add basic web server to track login

--- login.js
+++ login.js
@@ -4,13 +4,31 @@ function init() {
     const button = document.getElementById("loginBtn");
 
     button.onclick = function() {
-        alert("logging in user");
-        window.location.href = "upload.html";
+        const usernameInput = document.getElementById("username");
+        const username = usernameInput.value;
+
+        const passwordInput = document.getElementById("password");
+        const password = passwordInput.value;
+
+        getUserInformation(username, password);
     };
 }
 
+function getUserInformation(username, password) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        const userId = xmlHttp.responseText;
+        alert(userId);
+    };
+
+    xmlHttp.open("GET", "http://localhost:8080", true); 
+    xmlHttp.send(null);
+}
+
+function displayTestCreationEnv() {
+    // Personalize page
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-      // - Code to execute when all DOM content is loaded. 
-      // - including fonts, images, etc.
     init();
 });
--- loginserver.py
+++ loginserver.py
@@ -1,5 +1,9 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import mysql.connector
+import sys
+import boto3
+import os
 
 hostName = "localhost"
 serverPort = 8080
@@ -8,6 +12,18 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def do_PUT(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
