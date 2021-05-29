6ffb9e8c2592923080e5420911565d77a02fa6b1
Gregory Wint <gregwint95@gmail.com>
2021-05-29 21:06:32 +0100

    Add tests

---
+++ server/server/tests.py
@@ -0,0 +1,19 @@
+import dbutils
+
+def test_getDbConnection():
+    assert True
+
+def test_getEmptyUserData():
+    assert True
+
+def test_getUserInfo():
+    assert True
+
+def test_addUserUpload():
+    assert True
+
+def test_test():
+    assert True
+
+if __name__ == "__main__":
+    print("Running tests for db accessor code")
