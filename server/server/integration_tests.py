415f617fd80186a11e1d7c780a0d2a4b71c89af2
Gregory Wint <gregwint95@gmail.com>
2021-05-30 01:19:34 +0100

    Add basic integration tests

--- server/server/integration_tests.py
+++ server/server/integration_tests.py
@@ -4,16 +4,27 @@ def test_getDbConnection():
     assert dbutils.getDbConnection()
 
 def test_getEmptyUserData():
-    assert True
+    assert dbutils.getEmptyUserData() == {} 
 
 def test_getUserInfo():
+
     assert True
 
 def test_addUserUpload():
     assert True
 
-def test_test():
-    assert True
+def test_areUserCredentialsValid_present():
+    testUsername = "username"
+    testPassword = "password"
+
+    assert dbutils.areUserCredentialsValid(testUsername, testPassword)
+
+def test_areUserCredentialsValid_missing():
+    testUsername = "notpresent"
+    testPassword = "notpresent"
+
+    assert not dbutils.areUserCredentialsValid(testUsername, testPassword)
+
 
 if __name__ == "__main__":
     print("Running tests for db accessor code")
