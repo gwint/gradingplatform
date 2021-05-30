90db492fc7393bfca64c7e838d476c3818915a67
Gregory Wint <gregwint95@gmail.com>
2021-05-30 01:52:37 +0100

    Add seperate module for higher level utility functions

---
+++ server/server/siteutils.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import pymysql.cursors
+from django.shortcuts import render
+from dbutils import getDbConnection, areUserCredentialsValid
+
+
+def tryLogin(request, username, password):
+    if areUserCredentialsValid(username, password):
+        return render(request, "upload.html", {"username": username})
+    else:
+        return render(request, "login.html", {})
+
+def getEmptyUserData():
+    return {}
